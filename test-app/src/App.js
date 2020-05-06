1b1b11bf2441b758778af3947d2c253cabf32023
David Mihal <david@dmihal.com>
2020-05-06 00:00:59 -0300

    Display plugins in Material list

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
 import plugins from '../../plugin-specs';
 
-function App() {
+const App = () => {
+  const [selectedPlugins, setSelectedPlugins] = useState([]);
+  const availablePlugins = plugins.filter(plugin => selectedPlugins.indexOf(plugin.id) === -1);
+
   return (
     <div>
       <h1>Burner Wallet Plugins</h1>
-      {plugins.map(plugin => (
-        <div>{plugin.name}</div>
-      ))}
+      <List>
+        {availablePlugins.map(plugin => (
+          <ListItem
+            key={plugin.id}
+            onClick={() => setSelectedPlugins([...selectedPlugins, plugin.id])}
+            button
+          >
+            <ListItemText primary={plugin.name} secondary={`${plugin.id} - ${plugin.package}`} />
+          </ListItem>
+        ))}
+      </List>
     </div>
   );
 }
